6d97b764b4178772e765f772084fa0d43fdec1f1
Dennis Lee <dennis@dennislee.xyz>
2024-02-03 13:24:50 -0800

    break out date handling

--- content_script.js
+++ content_script.js
@@ -24,8 +24,7 @@ setTimeout(async () => {
 
         schedule[i].finalExamDate = finalTimeFinder.get(dateKey).get(timeKey)
     }
-    console.log(schedule)
-    console.log(ics(parse()))
+    console.log(ics(schedule))
 
 }, 5000);
 
@@ -118,19 +117,29 @@ function t24 (ampm) {
   return t24.toString().padStart(4, '0')
 }
 
+function yyyymmdd (date) {
+  return (
+    date.getUTCFullYear() +
+    (date.getUTCMonth() + 1).toString().padStart(2, '0') +
+    date.getUTCDate().toString().padStart(2, '0')
+  )
+}
+
 function ics (courses) {
   ics = 'BEGIN:VCALENDAR\n'
 
   courses.forEach(e => {
+    startDt = new Date(e.startDt)
+    endDt = new Date(e.endDt)
     meetingTimeStart = t24(e.meetingTimeStart)
     meetingTimeEnd = t24(e.meetingTimeEnd)
     meetingPattern = e.meetingPattern.map(e => e.substring(0, 2).toUpperCase())
 
     ics += 'BEGIN:VEVENT\n'
     ics += `SUMMARY:${e.name}\n`
-    ics += `RRULE:FREQ=WEEKLY;BYDAY=${meetingPattern.join()};UNTIL=${e.endDt.replaceAll('-', '')}\n`
-    ics += `DTSTART;TZID=America/Los_Angeles:${e.startDt.replaceAll('-', '')}T${meetingTimeStart}00\n`
-    ics += `DTEND;TZID=America/Los_Angeles:${e.startDt.replaceAll('-', '')}T${meetingTimeEnd}00\n`
+    ics += `RRULE:FREQ=WEEKLY;BYDAY=${meetingPattern.join()};UNTIL=${yyyymmdd(endDt)}\n`
+    ics += `DTSTART;TZID=America/Los_Angeles:${yyyymmdd(startDt)}T${meetingTimeStart}00\n`
+    ics += `DTEND;TZID=America/Los_Angeles:${yyyymmdd(startDt)}T${meetingTimeEnd}00\n`
     ics += `DESCRIPTION:${e.name}\n`
     ics += 'END:VEVENT\n'
 
@@ -140,8 +149,8 @@ function ics (courses) {
 
     ics += 'BEGIN:VEVENT\n'
     ics += `SUMMARY:${e.name}\n`
-    ics += `DTSTART;TZID=America/Los_Angeles:${e.endDt.replaceAll('-', '')}T${finalStart}00\n`
-    ics += `DTEND;TZID=America/Los_Angeles:${e.endDt.replaceAll('-', '')}T${finalEnd}00\n`
+    ics += `DTSTART;TZID=America/Los_Angeles:${yyyymmdd(endDt)}T${finalStart}00\n`
+    ics += `DTEND;TZID=America/Los_Angeles:${yyyymmdd(endDt)}T${finalEnd}00\n`
     ics += `DESCRIPTION:${e.name}\n`
     ics += 'END:VEVENT\n'
   })
