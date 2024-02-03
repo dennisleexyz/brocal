8dd594d788f5fcea52be23c837bddc680e519ef8
Mohraiel <mohraielmatta@gmail.com>
2024-02-03 03:51:08 -0800

    basic layout for extension is ready

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
 const HtmlPlugin= require('html-webpack-plugin')
+const tailwindcss= require('tailwindcss')
+const autoprefixer=require('autoprefixer');
 
 module.exports = {
     mode: "development",
@@ -14,14 +16,25 @@ module.exports = {
                 use: "ts-loader",
                 test: /\.tsx$/,
                 exclude: /node_modules/
+            },
+            {
+                use:['style-loader', 'css-loader', {
+                    loader: 'postcss-loader',
+                    options: {
+                        postcssOptions: {
+                            ident: 'postcss',
+                            plugins: [tailwindcss,autoprefixer],
+                        }
+                    }
+                }],
+                test: /\.css$/i,
             }
         ]
     },
     plugins: [
         new CopyPlugin({
             patterns: [
-                { from: path.resolve('src/manifest.json'), to: path.resolve('dist') },
-                { from: path.resolve('src/assets/icon.png'), to: path.resolve('dist') },
+                { from: path.resolve('src/static'), to: path.resolve('dist') },
             ],
         }),
         new HtmlPlugin({
